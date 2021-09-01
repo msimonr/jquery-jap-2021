c0f935dc7755de5d458d7375bb0caca31637a1d3
msimonr <mauricio.simon.r@gmail.com>
2021-09-01 16:23:47 -0300

    ejemplo para diapo

--- ejemplos/js/ejemplo1.js
+++ ejemplos/js/ejemplo1.js
@@ -1,4 +1,37 @@
-//Agrega el evento 
+/*
+
+//Cuando carga todo el HTML
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //Al darle click
+    document.getElementById('agregar').addEventListener('click', function() {
+
+        //Creando el elemento img
+        let img = document.createElement('img');
+        img.setAttribute('src', 'img/pikachu.png');
+        img.style.width = '100px';
+
+        //Agregandolo al DOM
+        document.getElementById('contenedor').appendChild(img);
+    });
+});
+
+*/
+
+
+
+
+
+//Cuando carga todo el HTML
 $(document).ready(function() {
 
-});
+    //Al darle click
+    $('#agregar').click(function() {
+
+        //Creando el elemento img
+        let img = $('<img>', { 'src': 'img/pikachu.png' }).css('width', '100px');
+
+        //Agregandolo al DOM
+        $('#contenedor').append(img);
+    });
+})
