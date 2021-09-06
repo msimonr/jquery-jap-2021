2844b33f150c8eb5af83b9931b92271eb5626836
msimonr <mauricio.simon.r@gmail.com>
2021-09-06 02:34:14 -0300

    todo

--- ejemplos/js/ejercicio.js
+++ ejemplos/js/ejercicio.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     $('#agregar').click(function() { //Evento click en el boton.
         let tarea = $('input').val(); //Obtiene el valor del input.
         if (tarea !== '') {
-            let elem = $('<li>').html(tarea);
-            $('.lista').append(elem);
-            cantTareas++;
+            let elem = $('<li>').html(tarea); //Crea elemento li
+            $('.lista').append(elem); //Agrega elem a lista
+            cantTareas++; //Suma 1 a tareas activas
             if (cantTareas > 4) {
-                $('img').show();
+                $('img').show(); //muestra imagen
             }
         }
     });
